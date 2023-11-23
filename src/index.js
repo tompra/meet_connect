d3012cd9622f7ab82f613557aed4982238cf4f3f
tompra <tomprayon@gmail.com>
2023-11-23 18:45:46 +0100

    Atatus

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import * as atatus from 'atatus-spa';
+
 atatus.config('b4b6a07bd1454eff8879c4dc6d03b9be').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
